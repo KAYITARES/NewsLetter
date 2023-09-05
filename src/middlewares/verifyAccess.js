903403ac0ea47f029f2a964809646800faabe141
KAYITARES <cyntkayitare@gmail.com>
2023-09-05 16:46:32 +0200

    add

--- src/middlewares/verifyAccess.js
+++ src/middlewares/verifyAccess.js
@@ -8,18 +8,21 @@ const VerifyAccess=(req,res,next)=>{
    if(!token){
     return errorResponse(res,401,`No Token Provided`)
    }else{
-    // verify if token is valid
-    const verifyToken=jwt.verify(token,process.env.SECRET_KEY,{expiresIn:"1d"})
-    if(!verifyToken){
-        return errorResponse(res,401,`Invalid token`)
-    }else{
-        // console.log(verifyToken.role!=="admin")
-       if(verifyToken.role!=="admin"){
-       return errorResponse(res,401,`You don't have access`)
-       }else{
-        return next()
+    try {
+        const verifyToken=jwt.verify(token,process.env.SECRET_KEY,{expiresIn:"1d"})
+        if(verifyToken.role!=="admin"){
+            return errorResponse(res,401,`You don't have access`)
+            }
+             return next()
+            
+        
+    } catch (error) {
+       if(error.name=="JsonWebTokenError"){
+        return errorResponse(res,401,`Invalid Token`)
        }
     }
+
+  
 
    }
 
